55ec3fce141ade53842b441206147dd2ac2992f8
Jason Gill <jasonjayo2002@gmail.com>
2022-04-17 03:43:59 +0100

    enemy movement with physics engine

--- main.lua
+++ main.lua
@@ -6,11 +6,34 @@
 
 -- Your code here 
 
+local physics = require( "physics" )
+physics.start()
+physics.setGravity(0,0);
+
+
 local ui = display.newGroup()
 local titleText = display.newText({parent=ui, text="Rectangles's Revenge", x=display.contentCenterX,y=10});
 
--- local player = display.newRect(display.contentCenterX, display.contentCenterY, 100, 50);
--- player.fill = {255, 255, 255}
+-- DEBUG
+-- debug config options
+local debug = {
+    drawEnemyPlayerTriangles= true,
+    drawCollisionBoxes= true,
+    drawCentreIndicators = true,
+    drawBoundaryMarkers = true,
+}
+
+-- DEBUG
+if (debug.drawCollisionBoxes) then
+    physics.setDrawMode("hybrid")
+end
+
+-- PLAYER
+
+local player = display.newRect(display.contentCenterX, display.contentCenterY, 100, 50);
+player.fill = {255, 255, 255}
+
+physics.addBody(player,"kinematic");
 
 local moveTimers = { left, right, up, down };
 
@@ -58,36 +81,73 @@ Runtime:addEventListener("key", onKey)
 
 -- leftButton:addEventListener("tap", onMoveLeftTap)
 
--- enemies
 
 
+-- ENEMIES
 
--- boundary markers
-display.newLine(0,0, 0,display.contentHeight);
-display.newLine(display.contentWidth,0, display.contentWidth,display.contentHeight);
+local enemiesUi = display.newGroup()
+local indicatorsUi = display.newGroup()
 
--- triangle
-local triangle = {0,0, 100,200, -100,200}
+if (debug.drawBoundaryMarkers) then
+    display.newLine(0,0, 0,display.contentHeight);
+    display.newLine(display.contentWidth,0, display.contentWidth,display.contentHeight);
+end
 
--- square
-local square = {0,0, 100,0, 100,100, 0,100}
-
--- pentagon
-local pentagon = {0,0, 80,60, 60,150, -60,150, -80,60}
--- local a = display.newPolygon(0+100, display.contentHeight/2, pentagon);
--- a:scale(0.4, 0.4)
--- a:setFillColor(245/255, 40/255, 145/255)
-
-
-local spawningEnemies = {triangle, square, pentagon}
+local spawningEnemies = {"triangle", "square", "pentagon"}
 local liveEnemies = {}
 
-for i = 1, 5, 1 do
+-- enemy properties 
+local enemies = {
+    triangle={
+        vertices={0,-25, 30,25, -30,25},
+        colour={1,1,1},
+        name="triangle"
+    },
+    square={
+        strength=1,
+        damage=1,
+        vertices={-25,-25, 25,-25, 25,25, -25,25},
+        name="square",
+        colour={0.3,0.65,1}
+    },
+    pentagon= {
+        vertices={0,-30, 30,-10, 20,30, -20,30, -30,-10},
+        colour={245/255, 40/255, 145/255},
+        name="pentagon"
+    }
+}
+
+
+local paint  = {
+    type=  "image",
+    filename ="arrow.png"
+}
+
+-- DEBUG
+local playerIndicator
+local enemyIndicator 
+if (debug.drawCentreIndicators) then
+    playerIndicator = display.newCircle(player.x, player.y, 5);
+    enemyIndicator = display.newCircle(0,0,5)
+end
+
+function spawnEnemy()
     local index = math.random(1, #spawningEnemies)
-    local a = display.newPolygon(math.random(50, 500), math.random(50, 250), spawningEnemies[index]);
-    table.insert(liveEnemies, a)
-    a:scale(0.4, 0.4)
-    a:setFillColor(245/255, 40/255, 145/255)
+    local shape = spawningEnemies[index]
+    local enemy = display.newPolygon(enemiesUi, math.random(0, display.contentWidth), math.random(-100, -50), enemies[shape].vertices);
+    table.insert(liveEnemies, enemy)
+
+
+    enemy:setFillColor(unpack(enemies[shape]["colour"]))
+    enemy.fill = paint
+
+    physics.addBody(enemy, "dynamic", {density=1, frction=1, bounce=0.8, shape=enemies[shape]["vertices"]})
+    -- physics.pause()
+
+    local l = player.x - enemy.x
+    local h = player.y - enemy.y
+    local hypot = math.sqrt(math.pow(l, 2) + math.pow(h, 2))
+
 end
 
 local direction = {x=1,y=1}
@@ -96,28 +156,72 @@ function setDirection()
     direction = {x=math.random(-1,1),y=math.random(-1,1)}
 end
 
-
+local enemyPlayerTriangles = {}
 
 function updateEnemies()
+    
+    -- DEBUG
+    if (debug.drawEnemyPlayerTriangles) then
+        for i = 1, #enemyPlayerTriangles, 1 do
+            enemyPlayerTriangles[i]:removeSelf()
+        end
+        enemyPlayerTriangles = {}
+    end
+
+
     for i = 1, #liveEnemies, 1 do
-        liveEnemies[i].x = liveEnemies[i].x + (math.random(1, 10) * direction["x"])
-        liveEnemies[i].y = liveEnemies[i].y + (math.random(10, 10) * direction["y"])
+
+        local l = (player.x - liveEnemies[i].x)
+        local h = (player.y - liveEnemies[i].y)
+        local hypot = (math.sqrt(math.pow(l, 2) + math.pow(h, 2)))
+
+        -- DEBUG
+        if (debug.drawEnemyPlayerTriangles) then
+            table.insert(enemyPlayerTriangles, display.newLine(liveEnemies[i].x, liveEnemies[i].y, player.x, player.y, liveEnemies[i].x, player.y, liveEnemies[i].x, liveEnemies[i].y))
+        end
+
+        local angle = math.asin(l / hypot);
+
+        if (h < 0) then
+            liveEnemies[i].rotation = math.deg(angle)
+        else
+            transition.to(liveEnemies[i], {time=500, rotation = 180 - math.deg(angle)})
+        end
+
+        liveEnemies[i]:setLinearVelocity(l / 10, h/10)
+
+        -- DEBUG
+        if (debug.drawCentreIndicators) then
+            enemyIndicator:removeSelf()
+            enemyIndicator = display.newCircle(liveEnemies[i].x, liveEnemies[i].y, 5)
+            enemyIndicator:setFillColor(0.65,1.00,0.30)
+            playerIndicator:removeSelf()
+            playerIndicator = display.newCircle(player.x, player.y, 5);
+            playerIndicator:setFillColor(0.5,0.5,0.5,1)
+        end
+        
     end
 end
 
+spawnEnemy()
 
-timer.performWithDelay(100, updateEnemies, 0);
+-- updateEnemies()
 
-timer.performWithDelay(1000, setDirection, 0);
+timer.performWithDelay(1000, updateEnemies, 0);
+
+timer.performWithDelay(5000, spawnEnemy, 0);
+
+-- local a = display.newPolygon(50, 50, enemies.pentagon.vertices);
+
+-- to be worked on later
+local state = { playerHealth=10 }
+
+local function restorePlayerHealth()
+    state.playerHealth = state.playerHealth + math.random(1, 5)
+end
 
 
-
-
-
-
-
-
-
+-- to be removed later
 
 local function polygon(sides, sideLength)
 
@@ -164,16 +268,3 @@ local function polygon(sides, sideLength)
     return vertices
 
 end
-
-
-
-
-
-
-local state = { playerHealth=10 }
-
-local function restorePlayerHealth()
-    state.playerHealth = state.playerHealth + math.random(1, 5)
-end
-
-
